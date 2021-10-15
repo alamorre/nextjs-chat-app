15a0aac1cce0a7d1de7f004ee5e4f09d5d5848eb
Adam La Morre <alamorre@gmail.com>
2021-10-15 07:41:20 -0400

    ready for social input

--- pages/chats/index.js
+++ pages/chats/index.js
@@ -1,6 +1,31 @@
+import React, { useState, useEffect } from 'react'
+
+import dynamic from 'next/dynamic'
+
+const ChatEngine = dynamic(() => import('react-chat-engine').then((module) => module.ChatEngine));
+
 export default function Home() {
-    return (
-      <div>chats</div>
-    )
-  }
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setTimeout(() => {
+        setLoaded(true)
+      }, 100)
+    }
+  }, [])
+
+  if (!loaded) return <div />
+
+  return (
+    <div style={{ padding: '100px', position: 'absolute', top: '0px', left: '0px', width: 'calc(100vw - 200px)', height: 'calc(100vh - 200px)', background: 'linear-gradient(150deg,#d9dbe2,#808aac 100%,#282d39 0)' }}>
+      <ChatEngine 
+        height='calc(100vh - 212px)'
+        projectID='52147d0e-0f43-4ea7-916f-1820a16bf1d7'
+        userName='Alice'
+        userSecret='pass1234'
+      />
+    </div>
+  )
+}
   
